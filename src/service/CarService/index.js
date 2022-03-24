3ae4050c7cf783e46f48955a889d464801ed5ffd
Bruno Moura <bruno8moura@gmail.com>
2022-03-24 05:34:32 -0300

    feat: given a carCategory, customer and numberOfDays it should calculate final amount in real

--- src/service/CarService/index.js
+++ src/service/CarService/index.js
@@ -1,8 +1,15 @@
 const BaseRepository = require('../../repository/BaseRepository')
-
+const Tax = require('../../entities/Tax')
 class CarService {
   constructor ({ cars }) {
     this.carRepository = new BaseRepository({ file: cars })
+
+    this.currencyFormat = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge
   }
 
   getRandomPositionArray (list) {
@@ -23,6 +30,18 @@ class CarService {
     const carId = this.chooseRandomCar(carCategory)
     const car = await this.carRepository.find(carId)
     return car
+  }
+
+  calculateFinalPrice (customer, carCategory, numberOfDays) {
+    const { age } = customer
+    const { price } = carCategory
+    const { then: tax } = this.taxesBasedOnAge
+      .find(tax => age >= tax.from && age <= tax.to)
+
+    const finalPrice = ((tax * price) * (numberOfDays))
+    const formattedPrice = this.currencyFormat.format(finalPrice)
+
+    return formattedPrice
   }
 }
 
