9891a90e1dceac7f646fd98903880aa73ee00dfa
Bruno Moura <bruno8moura@gmail.com>
2022-03-24 21:31:56 -0300

    chore: auto generate database and mocks

--- src/infra/seed/index.js
+++ src/infra/seed/index.js
@@ -4,6 +4,7 @@ const CarCategory = require('../../entities/CarCategory')
 const Customer = require('../../entities/Customer')
 const { join } = require('path')
 const seederBaseFolder = join(__dirname, '../', 'database')
+const mocksBaseFolder = join(__dirname, '../../../test', 'mocks')
 const { writeFile } = require('fs/promises')
 
 const ITEMS_AMOUNT = 2
@@ -37,13 +38,16 @@ for (let i = 0; i < ITEMS_AMOUNT; i++) {
   customers.push(customer)
 }
 
-const write = (filename, data) => writeFile(join(seederBaseFolder, filename), JSON.stringify(data))
+const write = (baseFolder, filename, data) => writeFile(join(baseFolder, filename), JSON.stringify(data))
 
 ;(async () => {
   console.log(seederBaseFolder)
-  await write('cars.json', cars)
-  await write('carCategory.json', [carCategory])
-  await write('customers.json', customers)
+  await write(seederBaseFolder, 'cars.json', cars)
+  await write(seederBaseFolder, 'carCategory.json', [carCategory])
+  await write(seederBaseFolder, 'customers.json', customers)
+  await write(mocksBaseFolder, 'valid-car.json', cars[0])
+  await write(mocksBaseFolder, 'valid-carCategory.json', carCategory)
+  await write(mocksBaseFolder, 'valid-customer.json', customers[0])
 
   console.log('cars', cars)
   console.log('carCategory', carCategory)
