5f921cb365f49b0804b8ec5f01b3a413d9a246a0
Bruno Moura <bruno8moura@gmail.com>
2022-03-26 12:39:19 -0300

    test: return a list when success listing categories

--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -1,6 +1,7 @@
 const { describe, it } = require('mocha')
 const request = require('supertest')
 const app = require('../../index')
+const assert = require('assert')
 
 describe('API Suite Test', () => {
   describe('calling /rentcar/categories', () => {
@@ -8,6 +9,14 @@ describe('API Suite Test', () => {
       await request(app)
         .get('/rentcar/categories')
         .expect(200)
+    })
+
+    it('should return a list when success listing categories', async () => {
+      const { body: { data } } = await request(app)
+        .get('/rentcar/categories')
+        .expect(200)
+
+      assert.ok(Array.isArray(data), true)
     })
   })
 })
