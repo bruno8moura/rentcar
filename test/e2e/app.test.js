17feaba58c7f5200edbcae7f1000409fcc25b8c4
Bruno Moura <bruno8moura@gmail.com>
2022-03-26 12:59:10 -0300

    test: return header location and content-type when success listing categories

--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -34,5 +34,18 @@ describe('API Suite Test', () => {
       assert.ok(Array.isArray(data), true)
       assert.deepStrictEqual(Object.keys(data[0]), expectedProperties)
     })
+
+    it('should return header location and content-type when success listing categories', async () => {
+      const { headers } = await request(app)
+        .get('/rentcar/categories')
+        .expect(200)
+
+      const expectedHeaders = {
+        location: '/rentcar/categories',
+        'content-type': 'application/json'
+      }
+
+      assert.ok(Object.keys(headers).includes(...Object.keys(expectedHeaders)))
+    })
   })
 })
